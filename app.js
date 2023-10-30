a68ac962a2e40a4061e809f734bab8f20553b031
anrunt <75736769+anrunt@users.noreply.github.com>
2023-10-30 01:00:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ function checkLocalStorage() {
     FocusTimeInput.value = localStorage.getItem("FocusTime");
   } else {
     FocusTimeInput.value = 30;
+    localStorage.setItem("FocusTime", FocusTimeInput.value);
   }
 }
 
